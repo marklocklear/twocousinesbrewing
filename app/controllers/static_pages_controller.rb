f4293369608c2b95aaaf0c4105bcd87b2991580e
Mark Locklear <marklocklear@gmail.com>
2016-02-17 12:28:18 -0500

    down and dirty single model to save email addy

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
+  	#danger - did not whitelist
+  	MailList.create email: params[:email]
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def forum_params
+      params.require(:MailList).permit(:email)
+    end
 end
---
+++ app/models/mail_list.rb
@@ -0,0 +1,3 @@
+class MailList < ActiveRecord::Base
+	validates :email, presence: true, uniqueness: true
+end
