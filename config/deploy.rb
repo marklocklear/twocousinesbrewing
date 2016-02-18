99f4df0133f7a2013c4bc1c6f26ad38c6df2005c
Mark Locklear <marklocklear@gmail.com>
2016-02-18 12:24:41 -0500

    fix deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,6 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{config/database.yml}
-set :linked_files, %w{config/secrets.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
